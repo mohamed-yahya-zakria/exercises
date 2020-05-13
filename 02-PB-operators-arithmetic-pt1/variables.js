9495d722e0ca37ca00228f6cfca1e33464c77521
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-13 15:51:16 +0200

    finished exercise

---
+++ 02-PB-operators-arithmetic-pt1/variables.js
@@ -0,0 +1,72 @@
+let x = 4;
+let y = 5;
+
+let overWriteX = 10;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(overWriteX / y); // i did this for i want integral number
+
+let z = 10;
+
+
+console.log(x * y / z);
+
+let resultOne = x * y / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a / b);
+
+let c = 20;
+
+console.log(c + b);
+
+let resultTwo;
+
+resultTwo = a + b;
+
+resultTwo = resultTwo * c;
+
+console.log(resultTwo);
+
+console.log(++a); // why when i write ++ don't work ,may be that for type of Variabl
+
+console.log(--b);
+
+let d = a - b;
+
+console.log(d + c);
+
+console.log(resultOne % resultTwo);
+
+
+
+
+
+/* Declare two variables "x" and "y" and assign them number values.
+
+Add x and y.
+
+Subtract y from x. Then, subtract x from y.
+
+Multiply x and y.
+
+Divide x by y.
+
+Declare another variable "z" with value "10". Multiply x and y. Then, divide the result by z. Store the result in a new variable named "resultOne". Print "resultOne" to the console.
+
+Declare two variables: "a" with value 15, and "b" with value 9. Divide a by b.
+
+Declare another variable "c" with value 20. Add a and b, then multiply the result by c. Store the result in variable "resultTwo". Print "resultTwo" to the console.
+
+Increment a.
+
+Decrement b.
+
+Subtract a from b and store this in a new variable "d". Add d and c.
+
+Print the remainder when "resultOne" is divided by "resultTwo" to the console. */
