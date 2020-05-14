1bbc3d0b9fb7053a1113a5d8060da237d66aa4ab
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-14 12:00:29 +0200

    ready Exercise03

---
+++ 03-PB-console/03-PB-consol.js
@@ -0,0 +1,24 @@
+console.log('dont forget quotaion marks');
+
+let tex = "This is  value";
+console.log(tex);
+
+let fristName = 'name';
+let lastName = 'Lname';
+console.log(fristName, lastName);
+
+let fname = 'ali';
+lastName = 'omar';
+let city = 'Cairo';
+let age = '52 years old';
+let idNum = 'the Id number is :' + 45877;
+console.log(fname, lastName, city, age, idNum);
+
+
+fname = 'John';
+lastName = 'smoth';
+city = 'berlin';
+age = '43 years old';
+let jop = 'Teacher';
+idNum = 'the Id number is :' + 45877;
+console.log('This is ', fname + lastName, 'he live in' + city, age, idNum);
