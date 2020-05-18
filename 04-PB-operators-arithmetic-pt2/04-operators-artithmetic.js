470d5beb98356c00fa6adafcd18dfece360fcab4
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-18 04:00:46 +0200

    ready exerice 05

--- 04-PB-operators-arithmetic-pt2/04-operators-artithmetic.js
+++ 04-PB-operators-arithmetic-pt2/04-operators-artithmetic.js
@@ -1,63 +1,60 @@
    //"use strict"; 
 
-allTickets = 6450 ;
-oneTicket = 15 ;
-console.log(allTickets / oneTicket);
+   allTickets = 6450;
+   oneTicket = 15;
+   console.log(allTickets / oneTicket);
 
-weekIncome = 500 ;
-NumOfweeks = 52 ;
-console.log(weekIncome * NumOfweeks);
+   weekIncome = 500;
+   NumOfweeks = 52;
+   console.log(weekIncome * NumOfweeks);
 
-x = 17 ;
-y = 30; 
-z = 100 ;
-console.log( ' namber is ' ,x / y * x, '%');
+   x = 17;
+   y = 30;
+   z = 100;
+   console.log(' namber is ', x / y * x, '%');
 
-perimeterSquare = 4.75 ;
-primeter = 4 ;
-console.log(perimeterSquare / primeter);
+   perimeterSquare = 4.75;
+   primeter = 4;
+   console.log(perimeterSquare / primeter);
 
-a = 5 ;
-b = 6 ;
-c = 7 ;
-p = (a +b + c);
-console.log(p);
-
-
-cube = (9 * 9 * 9);
-console.log(cube);
+   a = 5;
+   b = 6;
+   c = 7;
+   p = (a + b + c);
+   console.log(p);
 
 
-
-
-side = 5 ; 
-numSide = 4 ;
-console.log(side * numSide);
-
-x = 22.35 + .10*100;
-y = 26.67 + .15*100;
-d = 35.92 + .20*100;
-
-z = (x + y + d );
-
-
-numHour = (8 + 6 + 5 + 9 + 8 + 2 + 1);
-numberOfX = 10 ;
-
-console.log (numHour / numberOfX);
-
-scored = (75 + 70 + 85 + 90 + 100);
-num = 5 ; 
-average5 = (scored / num);
-averageTest6 = 85 ; 
-console.log(average5 + averageTest6);
-
-// acully i dont undrstand the question
-
-lastAverage = (.78 * 100);
+   cube = (9 * 9 * 9);
+   console.log(cube);
 
 
 
 
+   side = 5;
+   numSide = 4;
+   console.log(side * numSide);
+
+   x = 22.35 + .10 * 100;
+   y = 26.67 + .15 * 100;
+   d = 35.92 + .20 * 100;
+
+   z = (x + y + d);
 
 
+   numHour = (8 + 6 + 5 + 9 + 8 + 2 + 1);
+   numberOfX = 10;
+
+   console.log(numHour / numberOfX);
+
+   scored = (75 + 70 + 85 + 90 + 100);
+   num = 5;
+   average5 = (scored / num);
+   averageTest6 = 85;
+   console.log(average5 + averageTest6);
+
+   // acully i dont undrstand the question
+
+   // 12 
+   x = 78 * 8;
+   y = 80 * 9;
+   console.log(x * y);
---
+++ 05-PB-operators-comparison/comparison.js
@@ -0,0 +1,57 @@
+// task 1
+let x = 20;
+let y = 30;
+
+let taskOne = x === y 
+console.log( taskOne );
+
+// task 2
+let r = x !== y 
+console.log( r );
+
+//task 3
+let h = x > y ; 
+console.log( h );
+
+// task 4 
+let taskFour = x <= y ; 
+
+//task 5
+let taskFive = y > x ;
+
+//task 6
+let z = 5; 
+
+let a = x * z ;
+let q =  a > z ;
+let u = a + y ;
+
+console.log(a, u, q,);
+
+// task 7
+b = z - x ;
+let taskSeven = b < y ;
+c = b / z ;
+console.log (b, taskSeven, c );
+
+// task 8
+f = z && x === y ;
+console.log (f);
+
+// task 9
+
+p = x % z ;
+t = y % z ;
+let tasknine =  p === t ;
+console.log (p, t, tasknine );
+
+// task 10
+if (z + x > y - z ) {
+    console.log("yes z+y is greater");
+}
+else (x !== y)
+{ 
+    console.log("this is true");
+}
+
+
