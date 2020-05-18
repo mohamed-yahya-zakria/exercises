b44a7e00cd1aa09f87e8a6d1eda1a2dd1f7ec6c9
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-18 11:37:07 +0200

    mohame d exercise 05-ternary

---
+++ 06-PB-ternary-exercises/ternary.js
@@ -0,0 +1,40 @@
+// 01
+let dog = true;
+
+(dog === true) ? console.log ('pat, pat') : console.log('find me a dog to pat!');
+
+//02
+let speedCheck = 0;
+let speedlimit = 50;
+
+(speedCheck < speedlimit) ? console.log('youre going too fast!') : console.log('Youre driving below the speed limit, Oma');
+
+
+/* let speedCheck = 0;
+let speedlimit = 50;
+if(speedlimit > speedCheck) {
+    console.log('you are going too fast');
+}
+else (speedlimit < speedCheck) 
+{
+    console.log('you are driving below the speed limit');
+} */
+
+// 03
+let age = 16;
+
+(age < 16) ? console.log ('serve butter beer') : console.log('serve beer 0.Alc');
+
+// 04
+let isStudent = 5;
+
+if (isStudent >= 5) {
+    console.log('Ticket costs €5,00');
+}
+else {
+    console.log('Ticket costs €12,00');
+}
+
+//05
+let okMarie = 'cake';
+(okMarie === 'cake') ? console.log('Let them eat cake') : console.log('print "Oh, bother');
