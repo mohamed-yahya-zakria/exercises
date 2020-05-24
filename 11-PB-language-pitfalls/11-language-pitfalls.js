92d310ff4f6184b103df6a6c0fc790157e130768
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-24 20:42:47 +0200

    ready EX-11-language pitfalls-Mohamed

---
+++ 11-PB-language-pitfalls/11-language-pitfalls.js
@@ -0,0 +1,20 @@
+"use strikt";
+
+//01
+console.log(isNaN(`"hello`));
+console.log(isNaN(3));
+console.log(typeof(NaN));
+
+//What is the type of NaN? in fact is a Number value in JS
+
+//02 we use method (toFixed) to round the longer Number
+let rounding = (.1 * .2);
+console.log(rounding);
+console.log(rounding.toFixed(2));
+
+//03 the strange logic in JS without num1
+let num1 = (Infinity / 0);
+let num2 = (Infinity / Infinity); 
+let num3 = (1 / 0);
+
+console.log(`${num1}   ${num2}    ${num3}`);
