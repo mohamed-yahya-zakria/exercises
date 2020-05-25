8a3f2c9e3f30156321b56521b4df9c196f445291
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-25 10:35:27 +0200

    readyEX11-Mohamed

--- 11-PB-language-pitfalls/11-language-pitfalls.js
+++ 11-PB-language-pitfalls/11-language-pitfalls.js
@@ -1,14 +1,14 @@
-"use strikt";
+"use strict";
 
 //01
-console.log(isNaN(`"hello`));
+console.log(isNaN("hello"));
 console.log(isNaN(3));
 console.log(typeof(NaN));
-
+//NaN is not  a Number in js
 //What is the type of NaN? in fact is a Number value in JS
 
 //02 we use method (toFixed) to round the longer Number
-let rounding = (.1 * .2);
+let rounding = (0.1 * 0.2);
 console.log(rounding);
 console.log(rounding.toFixed(2));
 
