6bdc7af8d43ad0f2957dbfaba3dc8e5336e8c3ae
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-28 10:36:52 +0200

    why dont print anything

---
+++ 18-PB-conditionals-pt3/18-conditianls-pt3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let aleezaTeam = null;
+let lisTeam = null;
+if (aleezaTeam > lisTeam) {
+    aleezaTeam = (98 + 120 + 103);
+    lisTeam = (116 + 94 + 123);
+    console.log(`highest average score${aleezaTeam}`);
+} 
+else if (aleezaTeam < lisTeam) {
+    aleezaTeam = (98 + 120 + 103);
+    lisTeam = (116 + 94 + 123);
+    console.log(`highest average score${aleezaTeam}`); 
+}
