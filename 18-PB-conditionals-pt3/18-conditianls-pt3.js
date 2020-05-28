4b56e6fe607e951aa22117b3713feb650bb8d344
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-28 11:38:36 +0200

    complet EX18-mohamed

--- 18-PB-conditionals-pt3/18-conditianls-pt3.js
+++ 18-PB-conditionals-pt3/18-conditianls-pt3.js
@@ -1,16 +1,21 @@
 "use strict";
 
-let aleezaTeam = (9 + 120 + 103);
-let lisTeam = (116 + 94 + 1);
-let maryTeam = (9700 + 134 + 105);
+let aleezaTeam = (9 + 120 + 103 / 3);
+let lisTeam = (116 + 94 + 1 / 3);
+let maryTeam = (9700 + 134 + 105 / 3);
 if (aleezaTeam > lisTeam) {
     console.log(`highest average score is ${aleezaTeam} Aleeza team`);
-} 
-else if (aleezaTeam < lisTeam) {
-    console.log(`highest average score is lis team ${lisTeam}`); 
-   
+} else if (aleezaTeam === lisTeam) {
+    console.log(`the average score is samilar team aleeza and lis ${lisTeam} and ${aleezaTeam}`);
+} else if (aleezaTeam < lisTeam) {
+    console.log(`highest average score is lis team ${lisTeam}`);
+
+} else if (maryTeam > aleezaTeam && maryTeam > lisTeam) {
+    console.log(`the winner is mary team score is ${maryTeam}`);
 }
-   
-else if (maryTeam > aleezaTeam && maryTeam > lisTeam) {
-    console.log (`the winner is mary team score is ${maryTeam}`);
+else if (maryTeam === aleezaTeam || maryTeam === lisTeam) {
+    console.log (`mary team is like the same average Aleeza team or lis team`)
+}
+else {
+    console.log(`change the value of average to see th diffrent`)
 }
