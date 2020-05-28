ad11faa9a233dabd3ea988c09a7f3106e76a9a80
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-05-28 11:04:02 +0200

    complet EX18-mohamed

--- 18-PB-conditionals-pt3/18-conditianls-pt3.js
+++ 18-PB-conditionals-pt3/18-conditianls-pt3.js
@@ -1,14 +1,16 @@
 "use strict";
 
-let aleezaTeam = null;
-let lisTeam = null;
+let aleezaTeam = (9 + 120 + 103);
+let lisTeam = (116 + 94 + 1);
+let maryTeam = (9700 + 134 + 105);
 if (aleezaTeam > lisTeam) {
-    aleezaTeam = (98 + 120 + 103);
-    lisTeam = (116 + 94 + 123);
-    console.log(`highest average score${aleezaTeam}`);
+    console.log(`highest average score is ${aleezaTeam} Aleeza team`);
 } 
 else if (aleezaTeam < lisTeam) {
-    aleezaTeam = (98 + 120 + 103);
-    lisTeam = (116 + 94 + 123);
-    console.log(`highest average score${aleezaTeam}`); 
+    console.log(`highest average score is lis team ${lisTeam}`); 
+   
+}
+   
+else if (maryTeam > aleezaTeam && maryTeam > lisTeam) {
+    console.log (`the winner is mary team score is ${maryTeam}`);
 }
