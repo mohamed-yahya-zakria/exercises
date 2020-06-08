79f57738024bdb18be2164955752438db8d88d80
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-06-08 12:08:07 +0200

    eided thne name variable Ex 25

--- 25-PB-function-date/25-date-funcation.js
+++ 25-PB-function-date/25-date-funcation.js
@@ -3,16 +3,16 @@
 
 const currentDay = new Date();
 const userFutureDay = new Date('2020-7-2');
-
-const bookFutureDay = (today, futureday) => {
+let name = 'Mohamed';
+const bookFutureDay = (today, futureday, nameofuser) => {
     console.log(futureday);
     
     if ( futureday > today) {
-        console.log(`Your book to the event for ${futureday} is already guaranteed!`);
+        console.log(`Your book for name ${nameofuser} to the event for ${futureday} is already guaranteed!`);
     }
 
     else {
         console.log(`please choose futur date`);
     } 
 }
-bookFutureDay(currentDay, userFutureDay);
+bookFutureDay(currentDay, userFutureDay ,name);
