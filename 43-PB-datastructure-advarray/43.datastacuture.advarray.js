5872dccdf3bffc0a9578a9633e16fc0704e527b0
mohamed <mohamed.yahya.zakria73@gmail.com>
2020-07-02 08:20:36 +0200

    complete EX44

--- 43-PB-datastructure-advarray/43.datastacuture.advarray.js
+++ 43-PB-datastructure-advarray/43.datastacuture.advarray.js
@@ -1,35 +1,97 @@
 'use strict';
 
 //task 01
-const orders = [
-    { amount: 250 },
-    { amount: 400 },
-    { amount: 100 },
-    { amount: 325 }
-  ];
-const total = orders.reduce(function( previousValue, currentValue){
- return previousValue + currentValue
-}
+console.log('task 01')
+const orders = [{
+        amount: 250
+    },
+    {
+        amount: 400
+    },
+    {
+        amount: 100
+    },
+    {
+        amount: 325
+    }
+];
+const total = orders.reduce(function (previousValue, currentValue) {
+    console.log(previousValue);
+    return currentValue["amount"] + previousValue;
+    // we write 0 becouse tell him
+}, 0);
 console.log(total);
 
+console.log('task 02')
 //task 02
 //function that increments each element
-arrayOfNumbers = [3, 45, 6, 56, 7, 9]; 
+arrayOfNumbers = [3, 45, 6, 56, 7, 9];
+const incrementsElement = arrayOfNumbers.map(function (value) {
+    return value + 1;
+})
+console.log(incrementsElement);
 
-const incrementsElement = arrayOfNumbers.map(value) => value + 1);
- console.log(incrementsElement);
+console.log('task 03')
 
+//task 03
+const filtereven  = arr => arr.filter(value => value %2===0);
+console.log(filtereven([1, 2, 3, 11, 12, 13])) ;
+console.log(filtereven([22,2,31,110,6,13])) ;
+
+//task 03 why does not work is smilar above
+let filterEvens = [1, 2, 3, 11, 12, 13];
+let filterEvens2 = [22, 2, 31, 110, 6, 13];
+const evenNumbers = (arr) => {
+        return  arr.filter((value) {
+            return value  % 2 === 0; 
+        }) 
+}
+console.log(evenNumbers([1, 2, 3, 11, 12, 13]));
+console.log(evenNumbers(filterEvens2));
+
+console.log('task 04')
+// task 4
+const friends = ["rika", "jenna", "bleda", "oliver", "itamar"];
+const filterItems = (arr, searchTerm) =>{
+    let getChar = arr.filter(function(value){
+       if (value.toLowerCase().includes(searchTerm)) {
+        return value;
+       }
+       
+    })
+    return getChar;
+}
+console.log(filterItems(friends, "ka")); 
+console.log(filterItems(friends, "e")); 
+
+console.log('task 05')
 // task 5 
 //function called sum that uses the reduce method to sum up an array of numbers. 
+const sum = (arr) => arr.reduce((previous, current) => previous + current);
+console.log(sum([1, 2, 3, 4, 5])); // 15
+console.log(sum([6, 7, 7])); //20
 
-const sum = [1,2,3,4,5];
-sum.forEach(function(value, index, array)){
-  return  array[index]+value
+//task 05
+const sumNmber = (arr) =>{
+    arr.reduce((previous, current){
+        return previous + current
+    })
+    
 }
-console.log(sum);
+console.log(sumNmber([1, 2, 3, 4, 5])) ;
+console.log(sumNmber([6, 7, 7])); //20
 
-//task 06
-const myArray = [1,2,3,4,5];
- const result = myArray.map(function(value)) => Math.sqrt(value);
 
- console.log(result);
+
+//task 6
+const powArray = [1, 2, 3, 4, 5];
+const x = arr => arr.map(num => Math.sqrt(num));
+console.log(x(powArray));
+
+//task 06 why does not workis smilar above
+const myArray = [1, 2, 3, 4, 5];
+const result = myArray.map(function(value) => { 
+    return  Math.sqrt(value)})
+
+console.log(result);
+
---
+++ 44-PB-datastructure-logicalthinking/44-Data=stracture.logicalthinking.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function sortNumber (arr) {
+  return arr.sort() ;
+}
+console.log(sortNumber([4, 2, 5, 3, 9, 7, 1, 8, 2, 6]));
+
+//task 02
+
+const stringSort = (arr) => arr.sort();
+    
+console.log(stringSort(["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"]));
+
+
